9c90592a440231703e31ed130755c1ac5d8f3cdb
zacmelendez <zacmelendez@gmail.com>
2023-05-02 13:25:42 -0400

    Added sitemap and changed favicons

--- next.config.js
+++ next.config.js
@@ -2,13 +2,35 @@
 
 const path = require('path');
 const removeImports = require("next-remove-imports")();
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+});
 
-const nextConfig = removeImports({
+const nextConfig = withBundleAnalyzer(removeImports({
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
-})
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+    domains: ["localhost"],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  productionBrowserSourceMaps: true,
+
+}))
 
 module.exports = nextConfig
 
